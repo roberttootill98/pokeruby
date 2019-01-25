406096d3c2556329df5210156ab3c6bf30371649
Rob T <Robert.Tootill98@gmail.com>
2019-01-25 20:49:36 +0000

    added chansey to route 1 for testing physical special split

--- src/wild_encounter.c
+++ src/wild_encounter.c
@@ -1562,7 +1562,7 @@ const struct WildPokemonInfo SkyPillar_5F_LandMonsInfo = {10, SkyPillar_5F_LandM
 
 const struct WildPokemon Route101_LandMons [] =
 {
-    {2, 2, SPECIES_WURMPLE},
+    {100, 100, SPECIES_CHANSEY},
     {2, 2, SPECIES_ZIGZAGOON},
     {2, 2, SPECIES_WURMPLE},
     {3, 3, SPECIES_WURMPLE},
