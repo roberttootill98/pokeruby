eabb01ef12d834f0886ffc1ef67d53143ae6d403
Rob T <Robert.Tootill98@gmail.com>
2019-02-10 20:51:09 +0000

    add sleeth to route 1

--- src/wild_encounter.c
+++ src/wild_encounter.c
@@ -1563,8 +1563,8 @@ const struct WildPokemonInfo SkyPillar_5F_LandMonsInfo = {10, SkyPillar_5F_LandM
 const struct WildPokemon Route101_LandMons [] =
 {
     {100, 100, SPECIES_CHANSEY},
-    {2, 2, SPECIES_ZIGZAGOON},
-    {2, 2, SPECIES_WURMPLE},
+    {2, 2, SPECIES_SLEETH},
+    {2, 2, SPECIES_SLEETH},
     {3, 3, SPECIES_WURMPLE},
     {3, 3, SPECIES_ZIGZAGOON},
     {3, 3, SPECIES_ZIGZAGOON},
